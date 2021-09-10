6ebee4426e1f321bdbc9a0b64057356aebfa3438
joshmoak <joshmoak28@gmail.com>
2021-09-10 11:02:22 -0600

    1-4 done

--- StandardLibrary/standard_library.py
+++ StandardLibrary/standard_library.py
@@ -5,13 +5,17 @@
 <Date>
 """
 
+import calculator as cal
+from itertools import chain, combinations
 
 # Problem 1
 def prob1(L):
     """Return the minimum, maximum, and average of the entries of L
     (in that order).
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    print(min(L), max(L), (sum(L)/len(L)), sep = ",")
+
+    #raise NotImplementedError("Problem 1 Incomplete")
 
 
 # Problem 2
@@ -19,7 +23,48 @@ def prob2():
     """Determine which Python objects are mutable and which are immutable.
     Test numbers, strings, lists, tuples, and sets. Print your results.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    int_1 = 5
+    int_2 = int_1
+    int_2 = 3
+    if int_1 == int_2:
+        print("Numbers are mutable")
+    else:
+        print("Numbers are immutable")
+
+    str_1 = "test"
+    str_2 = str_1
+    str_2 = "testing"
+    if str_1 == str_2:
+        print("Strings are mutable")
+    else:
+        print("Strings are immutable")
+
+    list_1 = [1,2,3,4,5]
+    list_2 = list_1
+    list_2[0] = 0
+    if list_1 == list_2:
+        print("Lists are mutable")
+    else:
+        print("Lists are immutable")
+
+    tup_1 = (1,2,3,4,5)
+    tup_2 = tup_1
+    tup_2 = (0,2,3,4,5)
+    if tup_1 == tup_2:
+        print("Tuples are mutable")
+    else:
+        print("Tuples are immutable")
+
+    set_1 = {1,2,3,4,5}
+    set_2 = set_1
+    set_2 = {0,2,3,4,5}
+    if set_1 == set_2:
+        print("Sets are mutable")
+    else:
+        print("Sets are immutable")
+    
+
+    #raise NotImplementedError("Problem 2 Incomplete")
 
 
 # Problem 3
@@ -34,7 +79,9 @@ def hypot(a, b):
     Returns:
         The length of the triangle's hypotenuse.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    hypotenuse = cal.sqrt(cal.sum(cal.product(a,a),cal.product(b,b)))
+    return hypotenuse
+    # raise NotImplementedError("Problem 3 Incomplete")
 
 
 # Problem 4
@@ -47,7 +94,13 @@ def power_set(A):
     Returns:
         (list(sets)): The power set of A as a list of sets.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+
+    s = list(A)
+    results = list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))
+    return results
+
+
+    #raise NotImplementedError("Problem 4 Incomplete")
 
 
 # Problem 5: Implement shut the box.
